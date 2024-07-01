de9973cb3cb78859da3d3a93ea47ddc60eed460b
Malay Kumar <malaykumar2003@gmail.com>
2024-07-01 12:40:01 +0530

    Feat: actions and hooks init

---
+++ hooks/useRoutes.ts
@@ -0,0 +1,39 @@
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { Icons } from "@/components/Icons";
+import useConversation from "./useConversation";
+import path from "path";
+import { signOut } from "next-auth/react";
+
+const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Chat",
+        href: "/conversations",
+        icon: Icons.messageSquareMore,
+        active: pathname === "/conversations" || !!conversationId,
+      },
+      {
+        label: "Users",
+        href: "/users",
+        icon: Icons.users,
+        active: pathname === "/users",
+      },
+      {
+        label: "Log Out",
+        href: "#",
+        icon: Icons.logOut,
+        onClick: () => signOut(),
+      },
+    ],
+    [pathname, conversationId]
+  );
+
+  return routes;
+};
+
+export default useRoutes;
